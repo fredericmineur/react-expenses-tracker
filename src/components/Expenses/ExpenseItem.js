738d7e408b8cf130682b82f974c5a3d030ba1e9a
Frederic Mineur <fredericmineur@gmail.com>
2021-09-12 22:31:14 +0100

    remove button from ExpenseItem; from stateful to stateless component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,23 +1,17 @@
+import React from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
-import { useState } from "react";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  const clickHandler = () => {
-    setTitle('new title')
-    // console.log(title);
-  };
   console.log('ExpenseItem evaluated');
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount} $</div>
       </div>
-      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 };
